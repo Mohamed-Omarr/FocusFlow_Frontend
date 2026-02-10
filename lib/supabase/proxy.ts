81337bf671f32203e2224a3e0b7ae493847c93ae
Mohamed Omar <mohombusiness@gmail.com>
2026-02-10 22:12:01 +0300

    modified

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-const PROTECTED_PAGES = ['home', 'settings', 'sessions','task-planner','ai-analytics','active-session','register','forgot-password'];
-const PUBLIC_PAGES = ['/login'];
+const PROTECTED_PAGES = ['home', 'settings', 'sessions','task-planner','ai-analytics','active-session','login','register','forgot-password'];
+const PUBLIC_PAGES = ['/'];
 
 export async function supabaseSessionProxy(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
