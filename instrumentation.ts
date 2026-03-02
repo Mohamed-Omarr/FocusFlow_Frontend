0a33126c577efe7ca0eef88fa39cbdbce594c21a
Mohamed Omar <mohombusiness@gmail.com>
2026-03-02 16:53:33 +0300

    removed  "LangfuseSpanProcessor"

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,14 +1,8 @@
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { LangfuseSpanProcessor } from "@langfuse/otel";
 
-export const langfuseSpanProcessor = new LangfuseSpanProcessor({
-  publicKey: process.env.LANGFUSE_PUBLIC_KEY!,
-  secretKey: process.env.LANGFUSE_SECRET_KEY!,
-  baseUrl: process.env.LANGFUSE_BASE_URL!,
-});
-
-const sdk = new NodeSDK({
-  spanProcessors: [langfuseSpanProcessor],
+export const sdk = new NodeSDK({
+  spanProcessors: [new LangfuseSpanProcessor()],
 });
 
 sdk.start();
